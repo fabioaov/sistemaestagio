1310db10d6f6b9426b54adf866fec92f4377b6ca
Fábio Vieira <fabioaov@outlook.com>
2024-03-17 22:49:56 -0300

    Correções de tradução

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,8 @@
 <footer class="flex-shrink-0 px-6 py-4">
     <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
-        <span>Made with</span>
-
-        <span>
-            <x-heroicon-s-heart class="w-6 h-6 text-red-500" />
-        </span>
-
-        <span>by</span>
-
-        <a
-            href="https://github.com/Kamona-WD"
-            target="_blank"
-            class="text-blue-600 hover:underline"
-        >
-            Ahmed Kamel
+        <span>Desenvolvido por</span>
+        <a href="https://github.com/fabioaov" target="_blank" class="text-blue-600 hover:underline">
+            Fábio Vieira
         </a>
     </p>
 </footer>
